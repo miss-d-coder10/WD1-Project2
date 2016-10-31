b8e945dc5991b50b985de82aa664f7a83b26e428
Oli <olimoorow@gmail.com>
2016-10-31 17:06:06 +0000

    form submission working

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -2,8 +2,11 @@
 
 var giggity = giggity || {};
 var $main = $('main');
+var markers = void 0;
 
 giggity.map = null;
+giggity.currentLat = null;
+giggity.currentLng = null;
 
 //BUILDING THE MAP IN THE MAP
 giggity.mapSetup = function () {
@@ -15,22 +18,22 @@ giggity.mapSetup = function () {
   };
 
   this.map = new google.maps.Map($mapDiv[0], mapOptions);
-  this.getEvents();
   this.createPartial('formContainer');
   setTimeout(function () {
     giggity.autoComplete();
+    giggity.formHandler();
   }, 1000);
 };
 
-giggity.getEvents = function () {
+giggity.getEvents = function (lat, lng, radius, eventcode) {
   $.ajax({
     url: '/api/events',
     method: "GET",
     data: {
-      lat: 51.489915,
-      lng: -0.137818,
-      radius: 5,
-      eventcode: "COMEDY",
+      lat: lat,
+      lng: lng,
+      radius: radius,
+      eventcode: eventcode,
       limit: 100
     }
   }).done(this.loopThroughEvents.bind(giggity));
@@ -45,6 +48,9 @@ giggity.loopThroughEvents = function (data) {
 //ADD FUNCTION WHICH DROPS THE MARKER ONTO THE MAP
 giggity.createMarker = function (eventObject) {
   var latLng = new google.maps.LatLng(eventObject.venue.latitude, eventObject.venue.longitude);
+
+  var markerId = Math.floor(Math.random() * 1000000);
+
   var marker = new google.maps.Marker({
     position: latLng,
     map: this.map
@@ -87,7 +93,7 @@ giggity.autoComplete = function () {
     searchBox.setBounds(map.getBounds());
   });
 
-  var markers = [];
+  markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function () {
@@ -105,9 +111,8 @@ giggity.autoComplete = function () {
         console.log("Returned place contains no geometry");
         return;
       }
-      console.log(place);
-      console.log(place.geometry.location.lat());
-      console.log(place.geometry.location.lng());
+      giggity.currentLat = place.geometry.location.lat();
+      giggity.currentLng = place.geometry.location.lng();
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -118,3 +123,36 @@ giggity.autoComplete = function () {
     map.fitBounds(bounds);
   });
 };
+
+giggity.formHandler = function () {
+  var $formContainer = $('.formContainer');
+  $formContainer.on("submit", '#event-selector', function (e) {
+    emptyMarkerArray();
+    var $form = $(this);
+    e.preventDefault();
+    var data = $form.serializeArray();
+    var lat = giggity.currentLat;
+    var lng = giggity.currentLng;
+    var radius = data[2].value;
+    var eventcode = data[3].value;
+    giggity.getEvents(lat, lng, radius, eventcode);
+  });
+};
+
+function emptyMarkerArray() {
+  if (markers !== null) {
+    markers = [];
+  }
+}
+
+// function setMapOnAll(map) {
+//         for (var i = 0; i < markers.length; i++) {
+//           console.log(markers[i]);
+//           markers[i].setMap(map);
+//         }
+//       }
+
+function deleteMarkers() {
+  setMapOnAll(null);
+  markers = [];
+}
--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,8 +1,10 @@
   const giggity = giggity || {};
   let $main = $('main');
+  let markers;
 
   giggity.map = null;
-
+  giggity.currentLat = null;
+  giggity.currentLng = null;
 
   //BUILDING THE MAP IN THE MAP
   giggity.mapSetup = function() {
@@ -14,22 +16,22 @@
     };
 
     this.map = new google.maps.Map($mapDiv[0], mapOptions);
-    this.getEvents();
     this.createPartial('formContainer');
     setTimeout(function(){
       giggity.autoComplete();
+      giggity.formHandler();
     }, 1000);
   };
 
-  giggity.getEvents = function() {
+  giggity.getEvents = function(lat, lng, radius, eventcode) {
     $.ajax({
       url: '/api/events',
       method: "GET",
       data: {
-        lat:51.489915,
-        lng:-0.137818,
-        radius:5,
-        eventcode:"COMEDY",
+        lat:lat,
+        lng:lng,
+        radius:radius,
+        eventcode:eventcode,
         limit:100
       }
     })
@@ -46,6 +48,9 @@
   //ADD FUNCTION WHICH DROPS THE MARKER ONTO THE MAP
   giggity.createMarker = function (eventObject) {
     let latLng = new google.maps.LatLng(eventObject.venue.latitude, eventObject.venue.longitude);
+
+    var markerId = Math.floor(Math.random() * 1000000);
+
     let marker = new google.maps.Marker({
       position: latLng,
       map: this.map
@@ -93,7 +98,7 @@ giggity.autoComplete = function(){
     searchBox.setBounds(map.getBounds());
   });
 
-  var markers = [];
+  markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
@@ -111,9 +116,8 @@ giggity.autoComplete = function(){
         console.log("Returned place contains no geometry");
         return;
       }
-      console.log(place);
-      console.log(place.geometry.location.lat());
-      console.log(place.geometry.location.lng());
+      giggity.currentLat = place.geometry.location.lat();
+      giggity.currentLng = place.geometry.location.lng();
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -124,3 +128,37 @@ giggity.autoComplete = function(){
     map.fitBounds(bounds);
   });
 };
+
+
+giggity.formHandler = function() {
+  let $formContainer = $('.formContainer');
+  $formContainer.on("submit", '#event-selector', function(e) {
+    emptyMarkerArray();
+    let $form = $(this);
+    e.preventDefault();
+    let data = $form.serializeArray();
+    let lat = giggity.currentLat;
+    let lng = giggity.currentLng;
+    let radius = data[2].value;
+    let eventcode = data[3].value;
+    giggity.getEvents(lat, lng, radius, eventcode);
+  });
+};
+
+function emptyMarkerArray(){
+  if(markers !== null) {
+    markers = [];
+  }
+}
+
+// function setMapOnAll(map) {
+//         for (var i = 0; i < markers.length; i++) {
+//           console.log(markers[i]);
+//           markers[i].setMap(map);
+//         }
+//       }
+
+function deleteMarkers(){
+  setMapOnAll(null);
+  markers = [];
+}
