266a41e42c919167234a0a7f963ff91f828ee122
Oli <olimoorow@gmail.com>
2016-10-31 16:02:50 +0000

    setting up autocomplete

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -3,6 +3,25 @@
 var giggity = giggity || {};
 var $main = $('main');
 
+giggity.map = null;
+
+//BUILDING THE MAP IN THE MAP
+giggity.mapSetup = function () {
+  var $mapDiv = $('#map');
+
+  var mapOptions = {
+    center: { lat: 51.5074, lng: -0.1278 },
+    zoom: 12
+  };
+
+  this.map = new google.maps.Map($mapDiv[0], mapOptions);
+  this.getEvents();
+  this.createPartial('formContainer');
+  setTimeout(function () {
+    giggity.autoComplete();
+  }, 1000);
+};
+
 giggity.getEvents = function () {
   $.ajax({
     url: '/api/events',
@@ -28,23 +47,9 @@ giggity.createMarker = function (eventObject) {
   var latLng = new google.maps.LatLng(eventObject.venue.latitude, eventObject.venue.longitude);
   var marker = new google.maps.Marker({
     position: latLng,
-    map: giggity.map
+    map: this.map
   });
   giggity.addInfoWindow(eventObject, marker);
-};
-
-//BUILDING THE MAP IN THE MAP
-giggity.mapSetup = function () {
-  var $mapDiv = $('#map');
-
-  var mapOptions = {
-    center: { lat: 51.5074, lng: -0.1278 },
-    zoom: 12
-  };
-
-  this.map = new google.maps.Map($mapDiv[0], mapOptions);
-  this.getEvents();
-  this.createPartial('formContainer');
 };
 
 //ADDING INFO WINDOW
@@ -69,5 +74,47 @@ giggity.createPartial = function (partial) {
   var data = "";
   $.get(load_from, data, function (data) {
     $('.' + partial).html(data);
+  });
+};
+
+giggity.autoComplete = function () {
+
+  var map = giggity.map;
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      console.log(place);
+      console.log(place.geometry.location.lat());
+      console.log(place.geometry.location.lng());
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 };
--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,6 +1,26 @@
   const giggity = giggity || {};
   let $main = $('main');
 
+  giggity.map = null;
+
+
+  //BUILDING THE MAP IN THE MAP
+  giggity.mapSetup = function() {
+    let $mapDiv = $('#map');
+
+    let mapOptions = {
+      center: { lat: 51.5074, lng: -0.1278 },
+      zoom: 12
+    };
+
+    this.map = new google.maps.Map($mapDiv[0], mapOptions);
+    this.getEvents();
+    this.createPartial('formContainer');
+    setTimeout(function(){
+      giggity.autoComplete();
+    }, 1000);
+  };
+
   giggity.getEvents = function() {
     $.ajax({
       url: '/api/events',
@@ -28,23 +48,9 @@
     let latLng = new google.maps.LatLng(eventObject.venue.latitude, eventObject.venue.longitude);
     let marker = new google.maps.Marker({
       position: latLng,
-      map: giggity.map
+      map: this.map
     });
     giggity.addInfoWindow(eventObject, marker);
-  };
-
-  //BUILDING THE MAP IN THE MAP
-  giggity.mapSetup = function() {
-    let $mapDiv = $('#map');
-
-    let mapOptions = {
-      center: { lat: 51.5074, lng: -0.1278 },
-      zoom: 12
-    };
-
-    this.map = new google.maps.Map($mapDiv[0], mapOptions);
-    this.getEvents();
-    this.createPartial('formContainer');
   };
 
   //ADDING INFO WINDOW
@@ -74,5 +80,47 @@ giggity.createPartial = function(partial){
   $.get(load_from, data, function(data)
   {
       $(`.${partial}`).html(data);
+  });
+};
+
+giggity.autoComplete = function(){
+
+  let map = giggity.map;
+  var input = document.getElementById('pac-input');
+  let searchBox = new google.maps.places.SearchBox(input);
+
+  map.addListener('bounds_changed', function(){
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      console.log(place);
+      console.log(place.geometry.location.lat());
+      console.log(place.geometry.location.lng());
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 };
