62035a9db2c520f9b1efd7824a7c62ab2a9f911a
Oli <olimoorow@gmail.com>
2016-10-31 15:04:10 +0000

    removing broken autocomplete

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -71,26 +71,3 @@ giggity.createPartial = function (partial) {
     $('.' + partial).html(data);
   });
 };
-
-$(function () {
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function () {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function () {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-  });
-});
