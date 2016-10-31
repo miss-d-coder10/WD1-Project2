4a1ac51da49af0116ee350ac565f515a216c5ef6
Oli <olimoorow@gmail.com>
2016-10-31 11:19:12 +0000

    getting ready to merge

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -32,25 +32,22 @@ googleMap.loopThroughEvents = function (data) {
 
 //BUILDING THE MAP IN THE MAP
 
+
+//ADD FUNCTION WHICH DROPS THE MARKER ONTO THE MAP
+googleMap.createMarker = function (eventObject) {
+  var latLng = new google.maps.Map(eventObject.lat, eventObject.lng);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: googleMap.map
+  });
+};
+
 googleMap.mapSetup = function () {
   var $mapDiv = $('#map');
 
   var mapOptions = {
     center: { lat: 51.5014, lng: 0.1419 },
     zoom: 14
-<<<<<<< HEAD
-  });
-
-  //ADD FUNCTION WHICH DROPS THE MARKER ONTO THE MAP
-  googleMap.createMarker = function (eventObject) {
-    var latLng = new google.maps.Map(eventObject.lat, eventObject.lng);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: googleMap.map
-    });
-  };
-});
-=======
   };
   this.map = new google.maps.Map($mapDiv[0], mapOptions);
   this.getEvents();
@@ -59,4 +56,3 @@ googleMap.mapSetup = function () {
 // });
 
 $(googleMap.mapSetup.bind(googleMap));
->>>>>>> 1bd513e06af8237c98d7fe512c7b740a4971c274
