1bd513e06af8237c98d7fe512c7b740a4971c274
Oli <olimoorow@gmail.com>
2016-10-31 11:11:47 +0000

    oop google map

--- public/js/dist/main.js
+++ public/js/dist/main.js
@@ -1,44 +1,48 @@
 'use strict';
 
-$(function () {
-  console.log('JQ working');
-  var apiKey = "db7f4c46a760159833430e5dd92e1711";
-  var requestRadius = '5';
-  var requestLat = "51.5074";
-  var requestLon = '0.1278';
+// $(() =>{
 
-  var $main = $('main');
+var $main = $('main');
 
-  var googleMap = googleMap || {};
+var googleMap = googleMap || {};
 
-  googleMap.getEvents = function () {
-    $.ajax({
-      url: '/api/events',
-      method: "GET",
-      data: {
-        lat: 51.5074,
-        lng: 0.1278,
-        radius: 5,
-        eventcode: "LIVE"
-      }
-    })
-    // .done((data) => {
-    //   console.log("data", data);
-    //
-    // });
-    .done(this.loopThroughEvents);
-  };
+googleMap.getEvents = function () {
+  $.ajax({
+    url: '/api/events',
+    method: "GET",
+    data: {
+      lat: 51.5074,
+      lng: 0.1278,
+      radius: 5,
+      eventcode: "LIVE"
+    }
+  })
+  // .done((data) => {
+  //   console.log("data", data);
+  //
+  // });
+  .done(this.loopThroughEvents);
+};
 
-  googleMap.loopThroughEvents = function (data) {
-    $.each(data.Object, function (index, eventObject) {
-      googleMap.createMarker(eventObject);
-    });
-  };
+googleMap.loopThroughEvents = function (data) {
+  $.each(data.Object, function (index, eventObject) {
+    googleMap.createMarker(eventObject);
+  });
+};
 
-  //BUILDING THE MAP IN THE MAP
+//BUILDING THE MAP IN THE MAP
+
+googleMap.mapSetup = function () {
   var $mapDiv = $('#map');
-  var map = new google.maps.Map($mapDiv[0], {
+
+  var mapOptions = {
     center: { lat: 51.5014, lng: 0.1419 },
     zoom: 14
-  });
-});
+  };
+  this.map = new google.maps.Map($mapDiv[0], mapOptions);
+  this.getEvents();
+};
+
+// });
+
+$(googleMap.mapSetup.bind(googleMap));
--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,9 +1,4 @@
-$(() =>{
-  console.log('JQ working');
-  const apiKey = "db7f4c46a760159833430e5dd92e1711";
-  const requestRadius = '5';
-  const requestLat = "51.5074";
-  const requestLon = '0.1278';
+// $(() =>{
 
   let $main = $('main');
 
@@ -35,10 +30,19 @@ $(() =>{
 
 
   //BUILDING THE MAP IN THE MAP
-  let $mapDiv = $('#map');
-  let map = new google.maps.Map($mapDiv[0], {
-    center: { lat: 51.5014, lng: 0.1419 },
-    zoom: 14
-  });
 
-});
+  googleMap.mapSetup = function() {
+    let $mapDiv = $('#map');
+
+    let mapOptions = {
+      center: { lat: 51.5014, lng: 0.1419 },
+      zoom: 14
+    };
+    this.map =new google.maps.Map($mapDiv[0], mapOptions);
+    this.getEvents();
+  };
+
+
+// });
+
+$(googleMap.mapSetup.bind(googleMap));
